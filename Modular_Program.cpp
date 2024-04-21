fdd08fd2c65d90c46dac261f984da061a97678ed
Sebastian Lee <96149632+sebiprograms@users.noreply.github.com>
2024-04-21 16:13:49 -0500

    Two new functions added, Menu, and Restart. This project is complet now.

--- Modular_Program.cpp
+++ Modular_Program.cpp
@@ -3,24 +3,26 @@
 #include <iomanip>
 #include <cmath>
 using namespace std;
+void restart();
 
-
-void convertdollar()
+void ConvertDollar()
 {
 	double Dollars, Pesos;
 	std::cout << "To convert Dollars to Pesos input the of Dollars you have: \n";
 	std::cin >> Dollars;
 	Pesos = Dollars * 17.1005;
 	std::cout << "You have " << fixed << setprecision(2) << Pesos << " Pesos.\n";
+	restart();
 }
 
-void convertftc()
+void ConvertFTC()
 {
 	double Fahrenheit = 0.0, Celsius = 0.0;
 	std::cout << "To convert fahrenheit to celsius input degress fahrenheit: \n";
 	std::cin >> Fahrenheit;
 	Celsius = (Fahrenheit - 32) * 5 / 9;
 	std::cout << "Degrees in celsius = " << Celsius << "\n";
+	restart();
 }
 
 void CountLetter()
@@ -34,6 +36,7 @@ void CountLetter()
 		Letter_Count = i;
 	}
 	std::cout << "There are " << Letter_Count + 1<< " amount of letters in the name. \n";
+	restart();
 }
 
 void RaiseNumber()
@@ -43,6 +46,7 @@ void RaiseNumber()
 	std::cin >> X >> Y;
 	Total = pow(X, Y);
 	std::cout << "The expression is equal to " << Total << "\n";
+	restart();
 }
 
 void EvenOdd()
@@ -57,12 +61,76 @@ void EvenOdd()
 	else if (Number % 2 != 0)
 	{
 		std::cout << "The number " << Number << " is odd. \n";
-	}	
+	}
+	restart();
 }
+void menu()
+{
+	int select = 0;
+	std::cout << "    Input a number to execute program  \n";
+	std::cout << "  [1] Convert Dollars to Pesos\n";
+	std::cout << "  [2] Convert Fahrenheit to Celsius\n";
+	std::cout << "  [3] Count letters of your name\n";
+	std::cout << "  [4] Raise a number x to the power of y\n";
+	std::cout << "  [5] Determine if number is even or odd\n";
+	std::cout << "  [6] Exit Program\n";
+	std::cin >> select;
+	if (select == 1)
+	{
+		ConvertDollar();
+	}
+	else if (select == 2)
+	{
+		ConvertFTC();
+	}
+	else if (select == 3)
+	{
+		CountLetter();
+	}
+	else if (select == 4)
+	{
+		RaiseNumber();
+	}
+	else if (select == 5)
+	{
+		EvenOdd();
+	}
+	else if (select == 6)
+	{
+		std::cout << "Quiting Program...\n";
+		exit;
+	}
+	else 
+	{
+		std::cout << "invalid input retry\n";
+		menu();
+	}
+}
+void restart()
+{
+	string yesno;
+	std::cout << "Enter yes or no if you'd like to select another program: \n";
+	std::cin >> yesno;
+	if (yesno == "yes")
+	{
+		menu();
+	}
+	else if (yesno == "no")
+	{
+	std:cout << "quitting program...\n";
+		exit;
+	}
+}
+
 
 int main()
 {
+	int run = 0;
+	std::cout << "        Welcome       \n";
+	std::cout << "Enter a number 1-6 to choose a program function: \n";
+	menu();
 
+	return 0;
 
 
 
